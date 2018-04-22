fbe61bb2e4c252d89eb5b894609314f5438aceb1
Luis <ls.gontad@gmail.com>
2018-04-22 17:12:26 -0300

    vamos de cero_Carga Alumno ok

--- EjemploEstructura/main.c
+++ EjemploEstructura/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define TAM 5
 
 typedef struct
 {
@@ -10,16 +12,82 @@ typedef struct
   char nombre[50];
   int estado;
 
-}eAlumnos;
+}eAlumno;
 
+eAlumno pedirAlumno(void);
+int buscarLugarLibre (eAlumno[], int); // devuelve entero le paso lugar me falta el alumno
+int buscarIndiceDelAlumno(eAlumno listado[], int legajo, int cantidad);
+int insertarAlumnoEnLaLista(eAlumno[]);
+eAlumno pedirAlumno(void); // carga los datos del alumno
+void mostrarListado (eAlumno listado[], int cantidad);
+float calcularPromedio (int, int);
+ordenarPorPromedio(eAlumno[], int cantidad);
+
+// main menu de usuarios
+// completar funciones faltantes
 
 int main()
 {
 
+    int i;
+    eAlumno listadoAlumnos;
 
-eAlumnos listadoAlumnos[5];
+    listadoAlumnos = pedirAlumno();
+    /*(i=0;i<TAM;i++)
+    {
+        listadoDelMain[i].estado=0; // inicializo todos los elementos de estado en 0
+    }
+    eAlumnos aluDelMain;
+
+    aluDelMain= pedirAlumno();
+
+    mostrarAlumno(aluDelMain);*/
+
+
 
 
 
     return 0;
 }
+
+/*void mostrarAlumno(eAlumnos aluAux )
+{
+    printf("el legajo es : %d",aluAux.legajo );
+void mostrarAlumnos(eAlumnos listado[], int TAM)
+{
+    int i;
+    for (i = 0; i < c5; i++)
+    {
+        mostrarAlumno(lista[i]);
+    }
+}*/
+
+eAlumno pedirAlumno()
+{
+    eAlumno listadoAlumnos;
+    printf("Ingrese Legajo: " );
+    scanf("%d", &listadoAlumnos.legajo);
+    printf("Ingrese Nombre del Alumno: ");
+    fflush(stdin);
+    gets(listadoAlumnos.nombre);
+    printf("Ingrese Nota Uno: ");
+    scanf("%d", &listadoAlumnos.notaUno);
+    printf("Ingrese Nota Dos: ");
+    scanf("%d", &listadoAlumnos.notaDos);
+    return listadoAlumnos;
+}
+/*
+int buscarLugarLibre (eAlumnos listado[], int tam)
+{
+    int indice = -1;
+    int i;
+    for (i=0;i<tam;i++)
+    {        if (listado[i].estado=0)
+        {
+            indice = i;
+            break;
+        }
+    }
+    return indice;
+}
+*/
